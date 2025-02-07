d83b3f741e85ed98f8ecf5d69c95483596d481c0
Igo Marcelino <geslucian@hotmail.com>
2025-02-07 11:48:36 -0300

    Cria o controller para login, cria oos import para adicionar usuarios e roles

---
+++ src/main/java/com/igomarcelino/treinando_jwt/dto/LoginRequestDTO.java
@@ -0,0 +1,4 @@
+package com.igomarcelino.treinando_jwt.dto;
+
+public class LoginRequestDTO {
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO TB_USER(user_login, user_password)VALUES('admin', '$2a$12$C0vwo.6NB7Pzb9uv4c5QzOFntMQKm3lKgBBrbTN.AH9TCHIwVrF7W');
+
+INSERT INTO TB_ROLES(role_name)VALUES('ADMIN');
+INSERT INTO TB_ROLES(role_name)VALUES('BASIC');
+
+INSERT INTO TB_USER_ROLES(id_user, id_role)VALUES(1,1);
